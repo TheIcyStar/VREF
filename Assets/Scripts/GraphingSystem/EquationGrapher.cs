a55bb57226b4d0949341767366147997123f2948
evb5593 <182671131+evb5593@users.noreply.github.com>
2025-03-14 16:08:14 -0400

    feat: implement logic for equation graphing in a single plane

--- Assets/Scripts/GraphingSystem/EquationGrapher.cs
+++ Assets/Scripts/GraphingSystem/EquationGrapher.cs
@@ -11,17 +11,35 @@ public class EquationGrapher : MonoBehaviour
     private IGraphRenderer graphRenderer;
     // graph settings passed in
     private GraphSettings graphSettings;
+    // set of input variables (independent variables)
+    private HashSet<GraphVariable> inputVars;
+    // single output var computed by the function (dependent variable)
+    private GraphVariable outputVar;
 
-    public void InitializeGraph(ParseTreeNode equationTree, IGraphRenderer renderer, GraphSettings settings) {
+    public void InitializeGraph(ParseTreeNode equationTree, IGraphRenderer renderer, GraphSettings settings, HashSet<GraphVariable> inputVars, GraphVariable outputVar) {
         this.equationTree = equationTree;
         this.graphRenderer = renderer;
         this.graphSettings = settings;
+        this.inputVars = inputVars;
+        this.outputVar = outputVar;
+
+        // eventually, scale the graph based on largest range first
+        // ScaleGraph();
+
         VisualizeGraph();
     }
 
+    /*private void ScaleGraph()
+    {
+        float maxRange = Mathf.Max(graphSettings.xMax - graphSettings.xMin, graphSettings.yMax - graphSettings.yMin, graphSettings.zMax - graphSettings.zMin);
+
+        // scale the graph object so that the largest dimension fits in view
+        // transform.localScale = Vector3.one * (1f / maxRange);
+    }*/
+
     // renders the graph using the respective renderer and settings
     private void VisualizeGraph() {
-        graphRenderer.RenderGraph(equationTree, graphSettings);
+        graphRenderer.RenderGraph(equationTree, graphSettings, inputVars, outputVar);
     }
 
     // updates the graph settings and re-renders the graph
