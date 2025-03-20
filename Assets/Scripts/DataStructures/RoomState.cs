b67f28d22ef4204feece70b64a3cfabf35b59485
TheIcyStar <TheIcyStar@users.noreply.github.com>
2025-03-20 14:20:39 -0400

    wip dump

---
+++ Assets/Scripts/DataStructures/RoomState.cs
@@ -0,0 +1,9 @@
+public class RoomStateData {
+    public GraphSettings settings;
+    public ParseTreeNode[] equations;
+}
+
+public class APIResponse {
+    public RoomStateData data;
+    public string error;
+}
