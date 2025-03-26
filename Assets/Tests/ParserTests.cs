259771d478fd28cc2f7ae336926ac304cb34cf8e
evb5593 <182671131+evb5593@users.noreply.github.com>
2025-03-26 12:14:24 -0400

    feat: add two parser test cases

--- Assets/Tests/ParserTests.cs
+++ Assets/Tests/ParserTests.cs
@@ -1,24 +1,68 @@
+using System;
 using System.Collections;
+using System.Collections.Generic;
 using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.TestTools;
 
 public class ParserTests
 {
-    // A Test behaves as an ordinary method
-    [Test]
-    public void NewTestScriptSimplePasses()
+    EquationParser parser;
+
+    [SetUp]
+    public void Setup()
     {
-        // Use the Assert class to test conditions
+        parser = new EquationParser();
     }
 
-    // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
-    // `yield return null;` to skip a frame.
-    [UnityTest]
-    public IEnumerator NewTestScriptWithEnumeratorPasses()
+    [Test]
+    public void ParseAddition_ReturnsCorrectNodes()
     {
-        // Use the Assert class to test conditions.
-        // Use yield to skip a frame.
-        yield return null;
+        var tokens = new List<EquationToken>
+        {
+            new EquationToken("y", EquationParser.TYPE_VARIABLE),
+            new EquationToken("=", EquationParser.TYPE_RELOP),
+            new EquationToken("2", EquationParser.TYPE_NUMBER),
+            new EquationToken("+", EquationParser.TYPE_OPERATOR),
+            new EquationToken("3", EquationParser.TYPE_NUMBER)
+        };
+
+        var result = parser.Parse(tokens);
+
+        Assert.AreEqual("=", result.token.text);
+        Assert.AreEqual(EquationParser.TYPE_RELOP, result.token.type);
+        Assert.AreEqual("y", result.left.token.text);
+        Assert.AreEqual(EquationParser.TYPE_VARIABLE, result.left.token.type);
+        Assert.AreEqual("+", result.right.token.text);
+        Assert.AreEqual(EquationParser.TYPE_OPERATOR, result.right.token.type);
+        Assert.AreEqual("2", result.right.left.token.text);
+        Assert.AreEqual(EquationParser.TYPE_NUMBER, result.right.left.token.type);
+        Assert.AreEqual("3", result.right.right.token.text);
+        Assert.AreEqual(EquationParser.TYPE_NUMBER, result.right.right.token.type);
+    }
+
+    [Test]
+    public void ParseParenthesis_ReturnsNoParenthesis()
+    {
+        var tokens = new List<EquationToken>
+        {
+            new EquationToken("y", EquationParser.TYPE_VARIABLE),
+            new EquationToken("=", EquationParser.TYPE_RELOP),
+            new EquationToken("sin", EquationParser.TYPE_FUNCTION),
+            new EquationToken("(", EquationParser.TYPE_LEFTPAREN),
+            new EquationToken("4", EquationParser.TYPE_NUMBER),
+            new EquationToken(")", EquationParser.TYPE_RIGHTPAREN)
+        };
+
+        var result = parser.Parse(tokens);
+
+        Assert.AreEqual("=", result.token.text);
+        Assert.AreEqual(EquationParser.TYPE_RELOP, result.token.type);
+        Assert.AreEqual("y", result.left.token.text);
+        Assert.AreEqual(EquationParser.TYPE_VARIABLE, result.left.token.type);
+        Assert.AreEqual("sin", result.right.token.text);
+        Assert.AreEqual(EquationParser.TYPE_FUNCTION, result.right.token.type);
+        Assert.AreEqual("4", result.right.right.token.text);
+        Assert.AreEqual(EquationParser.TYPE_NUMBER, result.right.right.token.type);
     }
 }
