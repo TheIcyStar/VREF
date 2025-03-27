7c9cef1d742bdf6fb742038f18d51ace68d1eabb
evb5593 <182671131+evb5593@users.noreply.github.com>
2025-03-27 15:04:17 -0700

    refactor: shift GraphManager functions to EquationGrapher

--- Assets/Scripts/GraphingSystem/EquationGrapher.cs
+++ Assets/Scripts/GraphingSystem/EquationGrapher.cs
@@ -7,7 +7,7 @@ using UnityEngine;
 public class EquationGrapher : MonoBehaviour
 {
     // the root node of the equation tree
-    public ParseTreeNode equationTree;
+    private ParseTreeNode equationTree;
     // renderer interface set to the correct renderer type
     private IGraphRenderer graphRenderer;
     // graph settings passed in
@@ -16,8 +16,9 @@ public class EquationGrapher : MonoBehaviour
     private HashSet<GraphVariable> inputVars;
     // single output var computed by the function (dependent variable)
     private GraphVariable outputVar;
-
-    private Vector3 originalPostion;
+    // store references to scripts and objects
+    public AxisRenderer axisRenderer;
+    public GameObject graphObj;
 
     // all graph settings UI elements
     public TMP_InputField xAxisMin;
@@ -31,13 +32,29 @@ public class EquationGrapher : MonoBehaviour
     public TMP_InputField zRotation;
     public TMP_InputField step;
 
-    public void InitializeGraph(ParseTreeNode equationTree, IGraphRenderer renderer, GraphSettings settings, HashSet<GraphVariable> inputVars, GraphVariable outputVar) {
+    // equation renderer types
+    // change to enums later
+    private const int TYPE_LINE = 0;
+    private const int TYPE_MESH = 1;
+
+    // does nothing w/ def lin color for now
+    public void InitializeGraph(ParseTreeNode equationTree, Material defaultLineColor, GraphSettings settings) 
+    {
+        // determine what equation type is by parsing the whole tree
+        var (equationType, inputVars, outputVar) = DetermineEquationType(equationTree);
+
         this.equationTree = equationTree;
-        this.graphRenderer = renderer;
         this.graphSettings = settings;
         this.inputVars = inputVars;
         this.outputVar = outputVar;
-        this.originalPostion = this.transform.position;
+
+        // pass in the gameobject (1st child, the "graph" object) to add renderers to
+        if(equationType == TYPE_LINE) graphRenderer = new LineGraphRenderer(graphObj.transform);
+        else                          throw new GraphEvaluationException("Unsupported equation type attempting to be graphed.");
+
+        axisRenderer.InitializeAxes();
+        axisRenderer.UpdateAxes(this.graphSettings);
+
         ScaleGraph();
         VisualizeGraph();
     }
@@ -49,10 +66,12 @@ public class EquationGrapher : MonoBehaviour
         float baseRange = 2f;
         float maxRange = Mathf.Max(graphSettings.xMax - graphSettings.xMin, graphSettings.yMax - graphSettings.yMin, graphSettings.zMax - graphSettings.zMin);
         float scaleFactor = baseRange / maxRange;
-        this.transform.localScale = Vector3.one * scaleFactor;
+        graphObj.transform.localScale = Vector3.one * scaleFactor;
+
+        float zOffset = -graphSettings.zMin * scaleFactor;
 
         // calculate the z offset so that graph spawns above ground
-        this.transform.position = originalPostion + new Vector3(0, -graphSettings.zMin * scaleFactor, 0);
+        graphObj.transform.localPosition = new Vector3(0, zOffset, 0);
     }
 
     // renders the graph using the respective renderer and settings
@@ -75,5 +94,66 @@ public class EquationGrapher : MonoBehaviour
 
         ScaleGraph();
         VisualizeGraph();
+    }
+
+    // determines the type of equation by analyzing the parse tree
+    // also returns the input and output variables
+    // ONLY WORKS WITH [output var] = [f(input var)] FOR NOW
+    private (int, HashSet<GraphVariable>, GraphVariable) DetermineEquationType(ParseTreeNode equationTree) {
+        // initialize a set to store all the input vars
+        HashSet<GraphVariable> inputVars = new HashSet<GraphVariable>();
+        GraphVariable outputVar;
+
+        if(equationTree == null || equationTree.token.text != "=") {
+            throw new GraphEvaluationException("Explicit equation not found.");
+        }
+
+        // find output var (left of equal sign)
+        outputVar = ConvertToGraphVariable(equationTree.left.token.text);
+
+        // intialize equation type
+        int equationType;
+
+        // add all input variables to the set (right of equal sign)
+        DetermineVariables(equationTree.right, inputVars);
+
+        // check to see if there is no explicit variable
+        if(inputVars.Contains(outputVar)) throw new GraphEvaluationException("Variable found on left and right hand side of equation.");
+
+        // this logic needs to change to support constants, parametrics, planes, etc... in the future
+        if (inputVars.Count == 1)                         equationType = TYPE_LINE;
+        else if (inputVars.Count == 2)                    equationType = TYPE_MESH;
+        else                                              throw new GraphEvaluationException("Unsupported variable amount in right hand side of equation.");
+
+        return (equationType, inputVars, outputVar);
+    }
+
+    // finds all the variables in the given tree and adds them to a set
+    // potentially could create the set during tree creation if this
+    // full tree traversal ends up being too costly
+    private void DetermineVariables(ParseTreeNode equationTree, HashSet<GraphVariable> variables) {
+        if (equationTree == null) return;
+
+        if (equationTree.token.type == EquationParser.TYPE_VARIABLE) {
+            variables.Add(ConvertToGraphVariable(equationTree.token.text));
+        }
+
+        DetermineVariables(equationTree.left, variables);
+        DetermineVariables(equationTree.right, variables);
+    }
+
+    // converts a string to a GraphVariable
+    // if we really want to optimize everything down the line
+    // we can change the tree to use an enum for every possible
+    // token to avoid using strings entirely
+    private GraphVariable ConvertToGraphVariable(string stringVar)
+    {
+        return stringVar switch
+        {
+            "x" => GraphVariable.X,
+            "y" => GraphVariable.Y,
+            "z" => GraphVariable.Z,
+            _ => throw new GraphEvaluationException("Unknown/missing variable attempting to be graphed.")
+        };
     }
 }
--- Assets/Scripts/GraphingSystem/GraphManager.cs
+++ Assets/Scripts/GraphingSystem/GraphManager.cs
@@ -13,9 +13,8 @@ public class GraphManager : MonoBehaviour
     [SerializeField] private float graphDisplacement = 4f;
     // distance from the ground the graph spawns above
     [SerializeField] private float lowerGraphMargin = .5f;
-    // dictionary mapping a graph gameobject to all its scripts
-    // to avoid having to find them using GetComponent
-    private Dictionary<GameObject, (EquationGrapher grapher, AxisRenderer axisRenderer)> graphs;
+    // dictionary that maps prefabs to their manager script
+    private Dictionary<GameObject, EquationGrapher> graphs;
 
     // global graph settings from options menu
     // storing default values for now
@@ -23,23 +22,16 @@ public class GraphManager : MonoBehaviour
     // default material of graph lines
     public Material defaultLineColor;
 
-    // keep track of graph count solely for naming
+    // keep track of graph count solely for naming (for now)
     private int graphCount;
-
-    // equation renderer types
-    private const int TYPE_LINE = 0;
-    private const int TYPE_MESH = 1;
 
     public void Start() {
         graphs = new();
         graphCount = 1;
     }
 
-    // creates the graph object and attaches the script
+    // creates the graph object
     public void CreateNewGraph(ParseTreeNode equationTree) {
-        // determine what equation type is by parsing the whole tree
-        var (equationType, inputVars, outputVar) = DetermineEquationType(equationTree);
-
         // create an instance of the prefab, place it past the UI, and name it
         GameObject graphPrefabObj = Instantiate(graphPrefab, this.transform);
         Vector3 forward = equationUITransform.forward;
@@ -50,91 +42,13 @@ public class GraphManager : MonoBehaviour
         graphPrefabObj.name = $"Graph {graphCount}";
         graphCount++;
 
-        // get the children objects
-        GameObject graphObj = graphPrefabObj.transform.GetChild(0).gameObject; // graph
-        GameObject axesObj = graphPrefabObj.transform.GetChild(1).gameObject;  // axes
-        GameObject gridObj = graphPrefabObj.transform.GetChild(2).gameObject;  // gridlines
+        // get the script once
+        EquationGrapher grapher = graphPrefabObj.GetComponent<EquationGrapher>();
 
-        // access the equation grapher class (manages the state of each individual graph,
-        // maybe the name of that script should be changed for clarity)
-        // as well as the axis renderer and gridline renderer
-        EquationGrapher grapher = graphObj.GetComponent<EquationGrapher>();
-        AxisRenderer axisRenderer = axesObj.GetComponent<AxisRenderer>();
-        // gridlines script here (maybe combine both?)
+        // initialize the graph
+        grapher.InitializeGraph(equationTree, defaultLineColor, globalGraphSettings);
 
-        // initialize renderer
-        IGraphRenderer renderer;
-
-        // pass in the gameobject to add renderers to
-        if(equationType == TYPE_LINE) renderer = new LineGraphRenderer(graphObj.transform);
-        else                          throw new GraphEvaluationException("Unsupported equation type attempting to be graphed.");
-
-        // add the graph object and its corresponding scripts to the dictionary
-        graphs[graphPrefabObj] = (grapher, axisRenderer);
-
-        axisRenderer.InitializeAxes();
-        axisRenderer.UpdateAxes(globalGraphSettings);
-        grapher.InitializeGraph(equationTree, renderer, globalGraphSettings, inputVars, outputVar);
-    }
-
-    // determines the type of equation by analyzing the parse tree
-    // also returns the input and output variables
-    // ONLY WORKS WITH [output var] = [f(input var)] FOR NOW
-    private (int, HashSet<GraphVariable>, GraphVariable) DetermineEquationType(ParseTreeNode equationTree) {
-        // initialize a set to store all the input vars
-        HashSet<GraphVariable> inputVars = new HashSet<GraphVariable>();
-        GraphVariable outputVar;
-
-        if(equationTree == null || equationTree.token.text != "=") {
-            throw new GraphEvaluationException("Explicit equation not found.");
-        }
-
-        // find output var (left of equal sign)
-        outputVar = ConvertToGraphVariable(equationTree.left.token.text);
-
-        // intialize equation type
-        int equationType;
-
-        // add all input variables to the set (right of equal sign)
-        DetermineVariables(equationTree.right, inputVars);
-
-        // check to see if there is no explicit variable
-        if(inputVars.Contains(outputVar)) throw new GraphEvaluationException("Variable found on left and right hand side of equation.");
-
-        // this logic needs to change to support constants, parametrics, planes, etc... in the future
-        if (inputVars.Count == 1)                         equationType = TYPE_LINE;
-        else if (inputVars.Count == 2)                    equationType = TYPE_MESH;
-        else                                              throw new GraphEvaluationException("Unsupported variable amount in right hand side of equation.");
-
-        return (equationType, inputVars, outputVar);
-    }
-
-    // finds all the variables in the given tree and adds them to a set
-    // potentially could create the set during tree creation if this
-    // full tree traversal ends up being too costly
-    private void DetermineVariables(ParseTreeNode equationTree, HashSet<GraphVariable> variables) {
-        if (equationTree == null) return;
-
-        if (equationTree.token.type == EquationParser.TYPE_VARIABLE) {
-            variables.Add(ConvertToGraphVariable(equationTree.token.text));
-        }
-
-        DetermineVariables(equationTree.left, variables);
-        DetermineVariables(equationTree.right, variables);
-    }
-
-    // converts a string to a GraphVariable
-    // if we really want to optimize everything down the line
-    // we can change the tree to use an enum for every possible
-    // token to avoid using strings entirely
-    private GraphVariable ConvertToGraphVariable(string stringVar)
-    {
-        return stringVar switch
-        {
-            "x" => GraphVariable.X,
-            "y" => GraphVariable.Y,
-            "z" => GraphVariable.Z,
-            _ => throw new GraphEvaluationException("Unknown/missing variable attempting to be graphed.")
-        };
+        // add the graph object to the dictionary
+        graphs.Add(graphPrefabObj,  grapher);
     }
 }
