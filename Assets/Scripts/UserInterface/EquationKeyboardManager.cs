36c9d3f45fbaa9580c4d82048671f950aec8eb48
evb5593 <182671131+evb5593@users.noreply.github.com>
2025-03-29 11:13:35 -0400

    chore: rename 'KeyboardManager' to 'EquationKeyboardManager'

--- Assets/Scripts/UserInterface/EquationKeyboardManager.cs
+++ Assets/Scripts/UserInterface/EquationKeyboardManager.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System;
 using UnityEngine.Rendering;
 
-public class KeyboardManager : MonoBehaviour
+public class EquationKeyboardManager : MonoBehaviour
 {
     // text field from the UI
     public TMP_InputField equationInput;
