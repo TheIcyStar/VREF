ffc1267d0a28bc19b03d5aafcea4140fb544c065
evb5593 <182671131+evb5593@users.noreply.github.com>
2025-03-29 09:32:40 -0400

    refactor: separate graph variable enum and delete old renderer class

--- Assets/Scripts/GraphingSystem/GraphVariable.cs
+++ Assets/Scripts/GraphingSystem/GraphVariable.cs
@@ -1,13 +1,8 @@
 using UnityEngine;
-using System.Collections.Generic;
-using System.Linq;
 
 // TODO: Do the following when refactoring all scripts
-// -move the graph variable enum to its own file
 // -change token type to an enum
 // -store token type in its own file
-// -store the interface in a separate file
-// -store LineGraphRenderer in a separate file
 
 // graph variable enum to avoid string comparison
 public enum GraphVariable {
