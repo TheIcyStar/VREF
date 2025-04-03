68ebcdda6b445ecb64fdb56e77859cb83afff329
evb5593 <182671131+evb5593@users.noreply.github.com>
2025-04-02 21:55:24 -0400

    feat: highlight gizmo faces when hovered over

--- Assets/Scripts/UserInterface/RotationGizmoFace.cs
+++ Assets/Scripts/UserInterface/RotationGizmoFace.cs
@@ -1,12 +1,29 @@
+using System.ComponentModel;
 using UnityEngine;
 
 public class RotationGizmoFace : MonoBehaviour
 {
     [SerializeField] private RotationGizmoController controller;
     [SerializeField] private Vector3 targetRotation;
+    [SerializeField] private MeshRenderer highlightRenderer;
+
+    public void Awake()
+    {
+        highlightRenderer.enabled = false;   
+    }
 
     public void OnSelectEntered()
     {
         controller.SetGraphRotation(targetRotation);
+    }
+
+    public void OnHoverEntered() 
+    {
+        highlightRenderer.enabled = true;
+    }
+
+    public void OnHoverExited()
+    {
+        highlightRenderer.enabled = false;
     }
 }
